48eada37a2eb931a8dbcdf9a12854f5bda0c7d29
Rishant-Burriwar <burriwarrishant@gmail.com>
2026-08-05 01:03:32 +0530

    Added schema

---
+++ src/models/booksModel.js
@@ -0,0 +1,33 @@
+import { model, Schema } from "mongoose";
+
+const bookSchema = new Schema({
+    book_id :{
+        type : Number,
+        unique:true,
+        required:true,
+        min : [1,"ID must be greater then 0"]
+    },
+    name: {
+        type:String,
+        maxLength : [15,"Name cannot be this long"],
+        required :true
+    },
+    author:{
+        type : String,
+        maxLength : [15,"Author name cannot be this long"],
+        required : true
+    },
+    price :{
+        type: Number,
+        required :true
+    },
+    status :{
+        type :Boolean,
+        default :true,
+        required :true
+    }
+});
+
+const Book = model("Book",bookSchema);
+
+export {Book}
